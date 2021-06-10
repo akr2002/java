21683f60f4aa355440c4eddf6103cb777f55034b
GNUxeava <bluenerd@protonmail.com>
2021-06-10 11:02:48 +0530

    DemoJoin - initial

---
+++ DemoJoin/NewThread.java
@@ -0,0 +1,32 @@
+// Using join() to wai tfor thr theads to finish
+
+class NewThread implements Runnable
+{
+	String name; 	// name of thread
+	Thread t;
+
+	NewThread(String threadname)
+	{
+		name = threadname;
+		t = new Thread(this, name);
+		System.out.println("New thread: " + t);
+	}
+
+	// This is the entry point for thread.
+	public void run()
+	{
+		try
+		{
+			for (int i = 5; i > 0; --i)
+			{
+				System.out.println(name + ": " + i);
+				Thread.sleep(1000);
+			}
+		}
+		catch(InterruptedException e)
+		{
+			System.out.println(name + " interrupted.");
+		}
+		System.out.println(name + " exiting.");
+	}
+}
