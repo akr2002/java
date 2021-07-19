c894cb079015cdecdda72e463cdd0b5c46d4b198
GNUxeava <bluenerd@protonmail.com>
2021-07-19 09:31:41 +0530

    Deadlock - initial

---
+++ Deadlock/Deadlock.java
@@ -0,0 +1,27 @@
+class Deadlock implements Runnable {
+	A a = new A();
+	B b = new B();
+	Thread t;
+
+	Deadlock() {
+		Thread.currentThread().setName("MainThread");
+		t = new Thread(this, "RacingThread");
+	}
+
+	void deadlockStart() {
+		t.start();
+		a.foo(b);	// get lock on a in this thread.
+		System.out.println("Back in main thread");
+	}
+
+	public void run() {
+		b.bar(a);	// get lock on b in other thread.
+		System.out.println("Back in othet thread");
+	}
+
+	public static void main(String args[]) {
+		Deadlock dl = new Deadlock();
+
+		dl.deadlockStart();
+	}
+}
